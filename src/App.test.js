a6e959380d1a076268afe54d74456f5ae28d4a81
Nishikanta Nayak <nishikantanayak.info@gmail.com>
2024-09-28 11:30:17 +0530

    new test succesful file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello, World!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hello, world!/i); // Case-insensitive match
   expect(linkElement).toBeInTheDocument();
 });
+
