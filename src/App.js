d1e201cc1a782aae27fe613a7f1f29297d28c4f1
Nishikanta Nayak <nishikantanayak.info@gmail.com>
2024-10-03 16:52:48 +0530

    i added nayak in my code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello, World!</h1>
-      <h1>Nishikanta here.. </h1>
+      <h1>Nishikanta Nayak here.. </h1>
       <h1>This project is for buisness optima..</h1>
     </div>
   );
